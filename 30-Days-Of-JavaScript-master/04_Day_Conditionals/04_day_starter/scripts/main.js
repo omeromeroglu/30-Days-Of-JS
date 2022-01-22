cfe6b4c89e637fcd2d6f4a847772f881e22fcbc8
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-01-22 11:06:58 +0300

    day 4

--- 30-Days-Of-JavaScript-master/04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 30-Days-Of-JavaScript-master/04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,42 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+/* alert('Open the browser console whenever you work on JavaScript')
+ */
+//level 1
+
+/* let yourAge = prompt("Enter your age")
+
+if (yourAge > 18){
+    alert("You are old enough to drive")
+
+}else {
+    alert(`You are left with ${18 - yourAge} years to drive `)
+} */
+
+let a = 4
+let b = 3
+
+/* if ( a >b) {
+    console.log(`${a} is greater than ${b}`)
+} else if(b>a) {
+    
+    console.log(`${b} is greater than ${a}`)
+} else {
+    console.log(`${a} is equal to ${b}`)
+}
+
+
+ console.log ((a < b) ? "ternary truthy" : "ternary falsy")
+ */
+
+//  console.log((a%2 === 0) ? "its even":"its odd")
+
+// Level 2
+
+let getDaysInMonth = function(month,year) {
+    // Here January is 1 based
+    //Day 0 is the last day in the previous month
+   return new Date(year, month, 0).getDate();
+};
+
+   console.log(getDaysInMonth(2, 2022));
